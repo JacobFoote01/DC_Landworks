f30c2c56a10635120212b665faf49b5437e0bc60
Jacob Foote <jacobjfoote01@gmail.com>
2024-05-15 15:15:40 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href='/' className='Logo'>
+          <img src='/Small_logo.jpg'/>
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
