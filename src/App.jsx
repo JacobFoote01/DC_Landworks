9c54b08cdba3c7249a224259ea70b5f0a4f70c80
Jacob Foote <jacobjfoote01@gmail.com>
2024-07-02 12:36:56 -0600

    added a few files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    {/* <TopContent/> */}
+    {/* <BottomContent/> */}
+    {/* <Footer/> */}
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+
+const Footer = () => {
+    return(
+
+
+    <div>Footer Content</div>
+    )
+}
+export default Footer
