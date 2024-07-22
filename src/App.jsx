41e134e35fa6bb3c99b10016ac5425e812f91b98
Jacob Foote <jacobjfoote01@gmail.com>
2024-07-22 14:03:51 -0600

    made a collapsible navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar.jsx'
-import Footer from './Footer.jsx'
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaFacebook } from "react-icons/fa";
 
 function App() {
 
   return (
     <>
     <Navbar/>
-    {/* <TopContent/> */}
-    {/* <BottomContent/> */}
-    {/* <Footer/> */}
+    <>
+    <h3>About</h3>
+    <p>Welcome to our Page! We are a Landscape and Tree service contractor serving Utah and select portions of Salt Lake County Utah. With 10+ years of experience and a strong education in the most up-to-date horticultural practices, we can help you create a space full of life and beauty!</p>
+    <h3>Contact Us</h3>
+    <a href='tel:+18019133046'><FaPhoneAlt /></a>
+    <a href='mailto:dclandworksut@gmail.com'><MdEmail /></a>
+    <a href='https://www.facebook.com/people/DC-Landworks/61557464499433/'><FaFacebook /></a>
+    </>
     </>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,21 +1,29 @@
-import { FaPhoneAlt } from "react-icons/fa";
+import React, { useState } from "react";
 import { MdEmail } from "react-icons/md";
-import { FaFacebook } from "react-icons/fa";
+import { FaBars, FaTimes, FaPhoneAlt, FaFacebook } from "react-icons/fa"
+import './Navbar.css'
 
 const Navbar = () => {
+  const[isOpen, setisOpen] = useState(false)
+
+  const toggleNav = () => {
+    setisOpen(!isOpen)
+  }
+
     return (
-        <div>
-            <div>
-        <a href='/' className='Logo'>
+        <nav>
+            <div href='/' className='Logo'>
           <img src='/Small_logo.jpg'/>
-        </a>
       </div>
-      <div>
+      <div className="link" onClick={toggleNav}> 
+        {isOpen ? <FaTimes /> : <FaBars />}
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
         <a href='tel:+18019133046'><FaPhoneAlt /></a>
         <a href='mailto:dclandworksut@gmail.com'><MdEmail /></a>
         <a href='https://www.facebook.com/people/DC-Landworks/61557464499433/'><FaFacebook /></a>
-      </div>
+        </ul>
         </div>
+        </nav>
     )
 }
 
