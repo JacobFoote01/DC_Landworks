5a5da9f5bb0da756ec4f501c42ca2591782d55d7
Jacob Foote <jacobjfoote01@gmail.com>
2024-07-23 13:03:36 -0600

    made a few changes to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
     <Navbar/>
-    <>
+    {/* <>
     <h3>About</h3>
     <p>Welcome to our Page! We are a Landscape and Tree service contractor serving Utah and select portions of Salt Lake County Utah. With 10+ years of experience and a strong education in the most up-to-date horticultural practices, we can help you create a space full of life and beauty!</p>
     <h3>Contact Us</h3>
     <a href='tel:+18019133046'><FaPhoneAlt /></a>
     <a href='mailto:dclandworksut@gmail.com'><MdEmail /></a>
     <a href='https://www.facebook.com/people/DC-Landworks/61557464499433/'><FaFacebook /></a>
-    </>
+    </> */}
     </>
   )
 }
