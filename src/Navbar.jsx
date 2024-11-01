e62dbb6e0fbef8d3252409ece514d1e2fd9840a7
Jacob Foote <jacobjfoote01@gmail.com>
2024-11-01 14:48:05 -0600

    added in react router to route to my pages.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,36 +1,25 @@
 import React from "react";
-import { MdEmail } from "react-icons/md";
-import { FaPhoneAlt, FaFacebook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="logo">
-                <a href='/'>
+                <Link to='/'>
                     <img src='/Small_logo.jpg' alt="Logo"/>
-                </a>
+                </Link>
             </div>
             <div className="links">
-                    <a href="About">About</a>
-                    <a href="Services">Services</a>
-                    <a href="Contact">Contact</a>
+                    <Link to="/About">About</Link>
+                    <Link to="/Services">Services</Link>
+                    <Link to="/Contact">Contact</Link>
             </div>
-            <ul className="nav-links">
-                <button className="quote" >Get a Quote</button>
-                <li>
-                    <a href='tel:+18019133046'><FaPhoneAlt /></a>
-                </li>
-                <li>
-                    <a href='mailto:dclandworksut@gmail.com'><MdEmail /></a>
-                </li>
-                <li>
-                    <a href='https://www.facebook.com/people/DC-Landworks/61557464499433/'><FaFacebook /></a>
-                </li>
-            </ul>
+            <button className="quote">Get a Quote</button>
         </nav>
     );
 };
 
 export default Navbar;
+
 
