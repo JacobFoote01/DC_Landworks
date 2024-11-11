1926c8c9e92f0bc4a9fd7ddd350a226f52310f00
Jacob Foote <jacobjfoote01@gmail.com>
2024-11-11 13:36:25 -0700

    added some pictures

--- src/Services.jsx
+++ src/Services.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import './Pages.css';
 
 const services = [
-    { id: 1, title: "Landscaping", imgSrc: "/Landscaping.jpg", description: "High-quality lawn care services to keep your grass green and healthy." },
-    { id: 2, title: "Tree Trimming", imgSrc: "/path/to/tree-trimming.jpg", description: "Professional tree trimming for safety and beauty." },
-    { id: 3, title: "Garden Design", imgSrc: "/path/to/garden-design.jpg", description: "Create a beautiful garden tailored to your space." },
-    { id: 4, title: "Irrigation Systems", imgSrc: "/path/to/irrigation.jpg", description: "Efficient irrigation systems to keep your plants hydrated." }
+    { id: 1, title: "Landscaping", imgSrc: "/Landscape.jpg", description: "High-quality lawn care services to keep your grass green and healthy." },
+    { id: 2, title: "Tree Trimming", imgSrc: "/Trimming.jpg", description: "Professional tree trimming for safety and beauty." },
+    { id: 3, title: "Garden Design", imgSrc: "/Sprinklers.jpg", description: "Create a beautiful garden tailored to your space." },
+    { id: 4, title: "Irrigation Systems", imgSrc: "/Irrigation.jpg", description: "Efficient irrigation systems to keep your plants hydrated." }
 ];
 
 const Services = () => {
