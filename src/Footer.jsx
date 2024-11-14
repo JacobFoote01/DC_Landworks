7866853e3a42d7e4d78143d7e3a1a55f18996221
Jacob Foote <jacobjfoote01@gmail.com>
2024-11-14 15:43:08 -0700

    changed some background colors and messed with the navbar

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,12 +2,12 @@ import { FaFacebookSquare } from "react-icons/fa";
 import './index.css'
 
 const Footer = () => {
-    return(
-    <div className="footer">
-        <a href="https://www.facebook.com/profile.php?id=61557464499433">
-       <FaFacebookSquare className="facebook_logo"/> 
-        </a>
-    </div>
+    return (
+        <div className="footer">
+            <a href="https://www.facebook.com/profile.php?id=61557464499433">
+                <FaFacebookSquare className="facebook_logo" />
+            </a>
+        </div>
     )
 }
 export default Footer
